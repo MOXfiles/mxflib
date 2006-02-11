72d0c3075d193966e1748a549b32315d1934cc62
matt-beard <>
2006-02-11 18:14:42 +0000

    Fix signed/unsigned comparisons

--- mxflib/mdtraits.cpp
+++ mxflib/mdtraits.cpp
@@ -1715,7 +1715,7 @@ void MDTraits_Label::SetString(MDValuePtr Object, std::string Val)
 	strncpy(ValueBuff, Val.c_str(), VALBUFF_SIZE -1);
 	const char *p = ValueBuff;
 
-	int Count = Object->GetData().Size;
+	UInt32 Count = Object->GetData().Size;
 	int Value = -1;
 	UInt8 *Data = new UInt8[Count];
 	UInt8 *pD = Data;
@@ -1863,7 +1863,7 @@ void MDTraits_UMID::SetString(MDValuePtr Object, std::string Val)
 	strncpy(ValueBuff, Val.c_str(), VALBUFF_SIZE -1);
 	const char *p = ValueBuff;
 
-	int Count = Object->GetData().Size;
+	UInt32 Count = Object->GetData().Size;
 	int Value = -1;
 	UInt8 *Data = new UInt8[Count];
 	UInt8 *pD = Data;
