a94b649b54f6eb94712053b2a562d1d600deba37
matt-beard <>
2006-02-11 16:15:40 +0000

    Allow plug-able MDTraits

--- mxflib/deftypes.h
+++ mxflib/deftypes.h
@@ -115,7 +115,17 @@ namespace mxflib
 	 */
 	int LoadTypes(TypeRecordList &TypesData);
 
-	const MDTraits* LookupTraits(const char* TraitsName);
+	//! Find the traits for a given type - DEPRECATED
+	/*! <b>DEPRECATED - Use MDTraits::Find()</B>
+	 */
+	inline MDTraitsPtr LookupTraits(const char* Name) { return MDTraits::Find(std::string(Name)); }
+
+	//! Disable automatic loading of built-in traits
+	/*! \note This needs to be called early as they may have already been loaded!
+	 *  \ret false if the traits have already been loaded (or already disabled)
+	 */
+	bool DisableBuiltInTraits(void);
+
 
 	/* Define macros for static type definitions */
 
